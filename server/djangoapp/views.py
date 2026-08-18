07a57dbc3a48a477590d3fcb5be1196238ab4dbc
SylvanoJonni <nokromanx@gmail.com>
2026-08-18 22:00:59 +0800

    Fix lint failures in Python and JavaScript CI jobs
    Add flake8/jshint config to match the codebase's existing style, remove
    unused imports/vars, replace bare except clauses, and fix indentation
    and line-length issues.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,13 +1,8 @@
 # Uncomment the required imports before adding the code
 from .models import CarMake, CarModel
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -51,9 +46,7 @@ def logout_request(request):
 @csrf_exempt
 @csrf_exempt
 def registration(request):
-    context = {}
-
-	# Load JSON data from the request body
+    # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -61,19 +54,20 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(
+            username=username, first_name=first_name, last_name=last_name,
+            password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName":username,"status":"Authenticated"}
@@ -122,8 +116,9 @@ def add_review(request):
         data = json.loads(request.body)
         try:
             response = post_review(data)
+            print(response)
             return JsonResponse({"status":200})
-        except:
+        except Exception:
             return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
         return JsonResponse({"status":403,"message":"Unauthorized"})
